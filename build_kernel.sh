57aaa60229c0ff55b8d111cf9e61d2d03c400e2a
moon <bestmjh47@naver.com>
2014-04-13 17:25:27 +0900

    Linaro GCC 4.7.4

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+TOOLCHAINPATH=/home/moon/toolchain/linaro-4.7.4/bin
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=$TOOLCHAINPATH/arm-gnueabi-
 
 make u1_kor_skt_defconfig
-make
+make -j15
+cp arch/arm/boot/zImage zImage
+rm -rf modules/*
+find -name '*.ko' -exec cp -av {} modules \;
+        for i in modules/*; do $TOOLCHAINPATH/arm-gnueabi-strip --strip-unneeded $i;done;\
